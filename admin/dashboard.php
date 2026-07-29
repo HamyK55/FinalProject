f34703c8472c34d6e5f5764f241971e82b776fae
Humza <uptospace222@gmail.com>
2026-07-28 20:22:17 -0400

    Added help center with helpful articles for admin functionality

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -33,7 +33,9 @@ $adminName = $_SESSION["user_name"] ?? "Admin";
 
         <div class="site-links">
             <a class="site-link" href="dashboard.php">Admin Dashboard</a>
+             <a class="site-link" href="../helpPages/help_center.html">Help Center</a>
             <a class="site-link" href="../user-pages/logout.php">Logout</a>
+           
             <span class="site-user-note">
                 Hi, <?= htmlspecialchars($adminName) ?>
             </span>
